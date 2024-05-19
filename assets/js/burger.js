881d5bd37c1c5657fa1121c30378e8e6658c438a
StrayFox <strayfox1@mail.ru>
2024-05-19 15:39:04 +0300

    feat: Added adaptation of styles from the 824px screen

---
+++ assets/js/burger.js
@@ -0,0 +1,6 @@
+const burger_btn = document.querySelector(".icon_burger");
+const mobile_menu = document.querySelector(".menu_content_mobile");
+
+burger_btn.addEventListener('click', function(event) {
+    mobile_menu.classList.toggle('menu_active')
+});
